b185f9ae9864a406ef41d62c9afab0108551d672
Serolar <jakabiton@yahoo.com>
2016-01-29 14:09:40 -0600

    start work on mod

---
+++ src/main/java/com/serolar/chaoslore/ChaosLore.java
@@ -0,0 +1,32 @@
+package com.serolar.chaoslore;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="ChaosLore", name="Chaotic Lore", version="1.7.10-0.1")
+public class ChaosLore 
+{
+	@Mod.Instance("ChaosLore")
+	public static ChaosLore instance;
+	
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		
+	};
+	
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		
+	};
+	
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		
+	}
+	
+}
